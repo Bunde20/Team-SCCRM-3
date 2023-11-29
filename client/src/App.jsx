39d6328edcef2c01a16fb416301535f74722b04f
Steven Bunde <stevenbunde2020@gmail.com>
2023-11-28 19:38:44 -0500

    Added components/images/pages folders, and changed css files

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,53 +1,15 @@
-import { useState } from 'react';
-import Card from './components/Card';
-import cardPhoto from './images/Swinteger.webp';
-import cardPhoto2 from './images/Varchar.webp';
-import cardPhoto3 from './images/Leafarray.webp';
-import { faDroplet } from '@fortawesome/free-solid-svg-icons';
-import { faFire } from '@fortawesome/free-solid-svg-icons';
-import { faLeaf } from '@fortawesome/free-solid-svg-icons';
-import './App.css';
+import { useState } from 'react'
+import { Outlet } from 'react-router-dom'
 
-const creatures = [
-  {
-    name: 'Swinteger',
-    icon: faDroplet,
-    cardPhoto: cardPhoto,
-    description: 'Squirtle\'s long lost brother',
-  },
-  {
-    name: 'Varchar',
-    icon: faFire,
-    cardPhoto: cardPhoto2,
-    description: 'Charmander\'s roided uncle',
-  },
-  {
-    name: 'Leafarray',
-    icon: faLeaf,
-    cardPhoto: cardPhoto3,
-    description: 'Bulbie\'s long lost cousin',
-  },
-];
+import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
     <>
-    <div className="d-flex flex-row">
-      {creatures.map((creature) => {
-        return (
-          <Card
-            name={creature.name}
-            icon={creature.icon}
-            cardPhoto={creature.cardPhoto}
-            description={creature.description}
-          />
-        );
-      })}
-      </div>
+      <Outlet />
     </>
-  );
+  )
 }
 
-export default App;
+export default App

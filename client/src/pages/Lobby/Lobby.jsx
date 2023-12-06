af3a9bff47599174cbb8e42b618e43844548534b
Chris DeHaan <christopherwdehaan@gmail.com>
2023-12-05 19:30:39 -0500

    added background and basic page layout to lobby

--- client/src/pages/Lobby/Lobby.jsx
+++ client/src/pages/Lobby/Lobby.jsx
@@ -1,5 +1,37 @@
+import BackButton from '../../components/BackButton/BackButton'
+import Paragraph from '../../components/Paragraph'
+import './Lobby.css'
+
 export default function Lobby() {
+    const lobbyText = [
+        {
+            id: 0,
+            text: "Here you can prepare for you adventure! Remember, you must choose 3 Prográmon to take with you. Choose wisely and good luck in there, adventurer!"
+        }
+    ]
+
+    function lobbyParagraphRender() {
+        return lobbyText.map((obj) => <Paragraph {...obj} key={obj.id} />)
+    }
+
     return (
-        <p>This is the pre-game selection screen.</p>
+        <>
+            <body className='lobby-bg'>
+                <div className='col-12 text-center'>
+                    <BackButton />
+                </div>
+                <h1 className='homeTitle'>Prepare for the Palace</h1>
+                <main className='col-10 mx-auto'>
+                    <div>
+                        {lobbyParagraphRender()}
+                    </div>
+                    <div className='text-center'>
+                        <button className='continue-btn-cstm rounded'>Continue</button>
+                    </div>
+
+                </main>
+            </body>
+        </>
+
     )
 }

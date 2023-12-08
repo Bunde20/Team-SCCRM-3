e422c727acdacc9f14c51af65f0bd3e61ff0321e
CambiG1123 <campbellgilliland@gmail.com>
2023-12-07 21:42:11 -0500

    fixed Alert message for failing to sign up

---
+++ client/src/components/AlertModal2.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
+
+function AlertModal2({
+  heading = "Uh oh!",
+  message = "Something went wrong. Try Again!",
+  show,
+  handleClose,
+  onClose = () => {},
+}) {
+  return (
+    <>
+      <Modal className="text-center" show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>{heading}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>{message}</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Got it!
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+}
+
+export default AlertModal2;

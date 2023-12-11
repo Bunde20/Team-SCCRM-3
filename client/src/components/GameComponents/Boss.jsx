6fd368be2f40276e978eb5f656d04b45a786be17
Elrond-Hubbard <136862666+Elrond-Hubbard@users.noreply.github.com>
2023-12-11 15:16:24 -0500

    added additional animations to various elements

--- client/src/components/GameComponents/Boss.jsx
+++ client/src/components/GameComponents/Boss.jsx
@@ -2,13 +2,14 @@ import React, { useState, useEffect } from "react";
 import ProgressBar from "react-bootstrap/ProgressBar";
 
 import "./Boss.css";
+import "animate.css"
 
 export default function Boss(props) {
   return (
     <div className="col-12 col-xl-3 mx-auto d-flex flex-column align-items-center">
       <h2 className="text-light">{props.bossName}</h2>
       <div>
-        <img className="boss-image img-fluid" src={props.bossImage} alt="" />
+        <img className="boss-image img-fluid animate__animated animate__zoomIn animate__slow animate__delay-3s" src={props.bossImage} alt="" />
       </div>
       <ProgressBar
         className="my-3 col-12"

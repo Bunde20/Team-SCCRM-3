60d7392e5574b7f015ed5e8032596fc5e895e256
Steven Bunde <stevenbunde2020@gmail.com>
2023-12-10 20:19:54 -0500

    Styling changes & Added timer to reset marketplace cards

--- client/src/pages/Marketplace/Marketplace.jsx
+++ client/src/pages/Marketplace/Marketplace.jsx
@@ -12,12 +12,13 @@ import Card from "../../components/Card";
 import PurchaseBtn from "../../components/PurchaseBtn";
 import BackButton from "../../components/BackButton/BackButton";
 import MarketplaceNav from "../../components/MarketplaceNav";
-import "./Marketplace.css";
+import Timer from "../../components/Timer/Timer";
 
 export default function Marketplace() {
   const [cards, setCards] = useState([]);
   const [currentUser, setCurrentUser] = useState({});
   const [userCoins, setUserCoins] = useState(0);
+  const [timerReachedZero, setTimerReachedZero] = useState(false);
 
   useEffect(() => {
     const fetchCards = async () => {
@@ -25,10 +26,8 @@ export default function Marketplace() {
         const response = await cardAPI.getAllCards();
         const allCards = response.data;
 
-        // Shuffle the cards array
         const shuffledCards = allCards.sort(() => Math.random() - 0.5);
 
-        // Select the first four cards
         const selectedCards = shuffledCards.slice(0, 4);
 
         setCards(selectedCards);
@@ -36,26 +35,36 @@ export default function Marketplace() {
         console.error("Error fetching cards:", error);
       }
     };
+
     fetchCards();
 
     userAPI.getOneUser(localStorage.getItem("currentUser")).then((res) => {
       setCurrentUser(res.data[0]);
       setUserCoins(res.data[0].coins);
     });
-  }, []);
+
+    if (timerReachedZero) {
+      // Reset the timerReachedZero
+      setTimerReachedZero(false);
+
+      // Fetch new cards after timer expires
+      fetchCards();
+    }
+  }, [timerReachedZero]);
 
   return (
     <>
       <div className="col-12 marketplace-bg">
+        <Timer setTimerReachedZero={setTimerReachedZero} />
         <div className="col-12 mx-auto text-center">
-          <BackButton text='Back to Home' />
+          <BackButton text="Back to Home" />
         </div>
         <h1 className="text-center align-items-center justify-content-center homeTitle">
           Buy
         </h1>
         <MarketplaceNav />
         <h3 className="text-center">COINS: {userCoins}</h3>
-        <div className="d-flex justify-content-center flex-row flex-wrap">
+        <div className="d-flex justify-content-center flex-row flex-wrap pb-4">
           {cards.map((creature, index) => (
             <div key={index}>
               <Card creature={creature} key={creature._id} />

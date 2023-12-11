b96604e34845401ed97a4e95d75ec36969255dd3
Chris DeHaan <christopherwdehaan@gmail.com>
2023-12-10 23:33:31 -0500

    updated the 3 word links in marketplace

--- client/src/components/MarketplaceNav.jsx
+++ client/src/components/MarketplaceNav.jsx
@@ -7,10 +7,10 @@ export default function MarketplaceNav() {
     <Navbar expand="lg" className="justify-content-center">
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse className="justify-content-center" id="basic-navbar-nav">
-        <Nav className="mr-auto">
-          <Nav.Link className=" d-flex justify-content-center" as={Link} to="/marketplace"><h4 className="text-dark">Buy</h4></Nav.Link>
-          <Nav.Link className=" d-flex justify-content-center" as={Link} to="/trade"><h4 className="text-dark">Trade</h4></Nav.Link>
-          <Nav.Link className=" d-flex justify-content-center" as={Link} to="/mydeck"><h4 className="text-dark">My Deck</h4></Nav.Link>
+        <Nav className="text-center col-8 mx-auto">
+          <Nav.Link className='col-lg-4 col-12 mx-auto' as={Link} to="/marketplace"><h4 className="paragraph-text text-white fs-1 text-lg-end">Buy</h4></Nav.Link>
+          <Nav.Link className='col-lg-3 col-12 mx-auto' as={Link} to="/trade"><h4 className="paragraph-text text-white fs-1">Trade</h4></Nav.Link>
+          <Nav.Link className='col-lg-4 col-12 mx-auto' as={Link} to="/mydeck"><h4 className="paragraph-text text-white fs-1 text-lg-start">My Deck</h4></Nav.Link>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
